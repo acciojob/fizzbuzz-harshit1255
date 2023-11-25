e7456edbbf24fe8dc58bd941d357103993ad0c27
harshit1255 <108613826+harshit1255@users.noreply.github.com>
2023-11-26 04:29:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+let astring = ''
+for(let i= 1;i<=20;i++)
+	{
+		if(i %3==0 && i%5==0)
+		{
+			astring+='FizzBuzz\n'
+		}
+		else if(i%3==0)
+		{
+			astring+='Fizz\n'
+		}
+		else if(i%5==0)
+		{
+			astring+='Buzz\n'
+		}
+		else
+			astring+=`${i}\n`
+			
+	}
+alert(astring )
